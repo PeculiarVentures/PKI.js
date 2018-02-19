fd160e399db1ae1758cf2fe8ba47b859ace9a178
microshine <microshine@mail.ru>
2018-02-19 16:55:21 +0300

    Rename rollup options

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ let pkg = require("./package.json");
 let external = Object.keys(pkg.dependencies);
 
 export default {
-    entry: pkg["jsnext:main"],
+    input: pkg["jsnext:main"],
     plugins: [],
     external: external,
-    targets: [
+    output: [
         {
-            dest: pkg.main,
+            file: pkg.main,
             format: "cjs"
         }
     ]
