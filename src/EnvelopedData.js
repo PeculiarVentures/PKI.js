9b0a7dcc0db35e5fcb19218d61e1c51354b48049
Gus Narea <gnarea@users.noreply.github.com>
2020-01-20 16:35:36 +0000

    Compare recipientCertificate's curveObject in EnvelopedData using duck typing

--- src/EnvelopedData.js
+++ src/EnvelopedData.js
@@ -760,7 +760,7 @@ export default class EnvelopedData
 			{
 				const curveObject = _this.recipientInfos[index].value.recipientCertificate.subjectPublicKeyInfo.algorithm.algorithmParams;
 				
-				if((curveObject instanceof asn1js.ObjectIdentifier) === false)
+				if(curveObject.constructor.name !== "ObjectIdentifier")
 					return Promise.reject(`Incorrect "recipientCertificate" for index ${index}`);
 				
 				const curveOID = curveObject.valueBlock.toString();
@@ -1259,9 +1259,9 @@ export default class EnvelopedData
 					
 				const curveObject = decryptionParameters.recipientCertificate.subjectPublicKeyInfo.algorithm.algorithmParams;
 					
-				if((curveObject instanceof asn1js.ObjectIdentifier) === false)
-					return Promise.reject(`Incorrect "recipientCertificate" for index ${index}`);
 					
+				if(curveObject.constructor.name !== "ObjectIdentifier")
+					return Promise.reject(`BADGER decrypt Incorrect "recipientCertificate" for index ${index}`);
 				curveOID = curveObject.valueBlock.toString();
 					
 				switch(curveOID)
