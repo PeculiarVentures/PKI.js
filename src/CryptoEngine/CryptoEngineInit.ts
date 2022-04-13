0d20d79e7398bd4fa863a7e7fac5615dce9a8cf7
microshine <microshine@mail.ru>
2022-04-13 18:16:24 +0300

    refuxtor: use default crypto.webcrypto for nodejs

--- src/CryptoEngine/CryptoEngineInit.ts
+++ src/CryptoEngine/CryptoEngineInit.ts
@@ -25,17 +25,24 @@ export function initCryptoEngine() {
         subtleObject = self.crypto.subtle;
 
 
+      common.engine.name = engineName;
       if (typeof subtleObject === "undefined") {
-        common.engine.name = engineName,
-          common.engine.crypto = cryptoObject,
-          common.engine.subtle = null;
+        common.engine.crypto = cryptoObject;
+        common.engine.subtle = null;
       }
       else {
-        common.engine.name = engineName,
-          common.engine.crypto = cryptoObject,
-          common.engine.subtle = new CryptoEngine({ name: engineName, crypto: self.crypto as any, subtle: subtleObject });
+        common.engine.crypto = cryptoObject;
+        common.engine.subtle = new CryptoEngine({ name: engineName, crypto: self.crypto as any, subtle: subtleObject });
       }
     }
+  }
+  if (typeof crypto !== "undefined" && "webcrypto" in crypto) {
+    // NodeJS ^15
+    const name = "NodeJS ^15";
+    common.engine.name = name;
+    const nodeCrypto = (crypto as any).webcrypto as Crypto;
+    common.engine.crypto = (nodeCrypto as any).webcrypto;
+    common.engine.subtle = new CryptoEngine({ name, crypto: nodeCrypto, subtle: nodeCrypto.subtle });
   }
 
   common.setEngine(common.engine.name, common.engine.crypto, common.engine.subtle);
