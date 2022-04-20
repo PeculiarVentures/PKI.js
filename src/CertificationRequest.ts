34e51786a5d6c63a438a5173852c0abdb6228e25
microshine <microshine@mail.ru>
2022-04-20 22:33:42 +0300

    docs: add examples to CertificationRequest

--- src/CertificationRequest.ts
+++ src/CertificationRequest.ts
@@ -33,15 +33,40 @@ const CLEAR_PROPS = [
 ];
 
 export interface ICertificationRequest {
+  /**
+   * Value being signed
+   */
   tbs: ArrayBuffer;
+  /**
+   * Version number. It should be 0
+   */
   version: number;
+  /**
+   * Distinguished name of the certificate subject
+   */
   subject: RelativeDistinguishedNames;
+  /**
+   * Information about the public key being certified
+   */
   subjectPublicKeyInfo: PublicKeyInfo;
+  /**
+   * Collection of attributes providing additional information about the subject of the certificate
+   */
   attributes?: Attribute[];
+
+  /**
+   * signature algorithm (and any associated parameters) under which the certification-request information is signed
+   */
   signatureAlgorithm: AlgorithmIdentifier;
+  /**
+   * result of signing the certification request information with the certification request subject's private key
+   */
   signatureValue: asn1js.BitString;
 }
 
+/**
+ * JSON representation of {@link CertificationRequest}
+ */
 export interface CertificationRequestJson {
   tbs: string;
   version: number;
@@ -109,6 +134,97 @@ export type CertificationRequestParameters = PkiObjectParameters & Partial<ICert
 
 /**
  * Represents the CertificationRequest structure described in [RFC2986](https://datatracker.ietf.org/doc/html/rfc2986)
+ *
+ * @example The following example demonstrates how to parse PKCS#11 certification request
+ * and verify its challenge password extension and signature value
+ * ```js
+ * const pkcs10 = pkijs.CertificationRequest.fromBER(pkcs10Raw);
+ *
+ * // Get and validate challenge password extension
+ * if (pkcs10.attributes) {
+ *   const attrExtensions = pkcs10.attributes.find(o => o.type === "1.2.840.113549.1.9.14"); // pkcs-9-at-extensionRequest
+ *   if (attrExtensions) {
+ *     const extensions = new pkijs.Extensions({ schema: attrExtensions.values[0] });
+ *     for (const extension of extensions.extensions) {
+ *       if (extension.extnID === "1.2.840.113549.1.9.7") { // pkcs-9-at-challengePassword
+ *         const asn = asn1js.fromBER(extension.extnValue.valueBlock.valueHex);
+ *         if (asn.result.valueBlock.value !== "passwordChallenge") {
+ *           throw new Error("PKCS#11 certification request is invalid. Challenge password is incorrect");
+ *         }
+ *       }
+ *     }
+ *   }
+ * }
+ *
+ * // Verify signature value
+ * const ok = await pkcs10.verify();
+ * if (!ok) {
+ *   throw Error("PKCS#11 certification request is invalid. Signature is wrong")
+ * }
+ * ```
+ *
+ * @example The following example demonstrates how to create PKCS#11 certification request
+ * ```js
+ * // Get a "crypto" extension
+ * const crypto = pkijs.getCrypto(true);
+ *
+ * const pkcs10 = new pkijs.CertificationRequest();
+ *
+ * pkcs10.subject.typesAndValues.push(new pkijs.AttributeTypeAndValue({
+ *   type: "2.5.4.3",
+ *   value: new asn1js.Utf8String({ value: "Test" })
+ * }));
+ *
+ *
+ * await pkcs10.subjectPublicKeyInfo.importKey(keys.publicKey);
+ *
+ * pkcs10.attributes = [];
+ *
+ * // Subject Alternative Name
+ * const altNames = new pkijs.GeneralNames({
+ *   names: [
+ *     new pkijs.GeneralName({ // email
+ *       type: 1,
+ *       value: "email@address.com"
+ *     }),
+ *     new pkijs.GeneralName({ // domain
+ *       type: 2,
+ *       value: "www.domain.com"
+ *     }),
+ *   ]
+ * });
+ *
+ * // SubjectKeyIdentifier
+ * const subjectKeyIdentifier = await crypto.digest({ name: "SHA-1" }, pkcs10.subjectPublicKeyInfo.subjectPublicKey.valueBlock.valueHex);
+ *
+ * pkcs10.attributes.push(new pkijs.Attribute({
+ *   type: "1.2.840.113549.1.9.14", // pkcs-9-at-extensionRequest
+ *   values: [(new pkijs.Extensions({
+ *     extensions: [
+ *       new pkijs.Extension({
+ *         extnID: "2.5.29.14", // id-ce-subjectKeyIdentifier
+ *         critical: false,
+ *         extnValue: (new asn1js.OctetString({ valueHex: subjectKeyIdentifier })).toBER(false)
+ *       }),
+ *       new pkijs.Extension({
+ *         extnID: "2.5.29.17", // id-ce-subjectAltName
+ *         critical: false,
+ *         extnValue: altNames.toSchema().toBER(false)
+ *       }),
+ *       new pkijs.Extension({
+ *         extnID: "1.2.840.113549.1.9.7", // pkcs-9-at-challengePassword
+ *         critical: false,
+ *         extnValue: (new asn1js.PrintableString({ value: "passwordChallenge" })).toBER(false)
+ *       })
+ *     ]
+ *   })).toSchema()]
+ * }));
+ *
+ * // Signing final PKCS#10 request
+ * await pkcs10.sign(keys.privateKey, "SHA-256");
+ *
+ * const pkcs10Raw = pkcs10.toSchema(true).toBER();
+ * ```
  */
 export class CertificationRequest extends PkiObject implements ICertificationRequest {
 
@@ -261,7 +377,7 @@ export class CertificationRequest extends PkiObject implements ICertificationReq
     }));
   }
 
-  public toSchema(encodeFlag = false): Schema.SchemaType {
+  public toSchema(encodeFlag = false): asn1js.Sequence {
     let tbsSchema;
 
     if (encodeFlag === false) {
