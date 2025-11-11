20e19d14893913ebe68b6716894fc5b1c40b94f9
Stefanie Weiß <stefanie.weiss@governikus.de>
2025-11-11 23:45:32 +0100

    Unit test for createCMSECDSASignature

--- test/asn1jIntTest.spec.ts
+++ test/asn1jIntTest.spec.ts
@@ -1,128 +1,42 @@
-import * as pkijs from "pkijs";
 import * as asn1js from "asn1js";
 import * as assert from "assert";
-import * as crypto from "crypto";
-import {getCrypto} from "../src";
-import {createPublicKey} from "node:crypto";
+import {createCMSECDSASignature} from "../src";
+import {Convert} from "pvtsutils";
 
+const testVectors = [
+    {
+        rHex: "010fc11f9fa733d62042904c2a4f5077d84a0cb61162e10dec784830521327a4a50d8be8a7b3778568a6c51355519ea12dad6347583c0ff6921285c7ed3db7439586",
+        sHex: "00008bde7dadfe9ce0563383ef6dc2f06a6cb35f72cc66b5626bf91a1508b309207390ff302164c2e78f7e2958ee8d9e9cc7cca4a6a20cb9a011b0850212a50ca515"
 
-it("demonstrate webcrypto verify error", async function () {
-    this.timeout(100000);
+    },
+    {   rHex: "00000000068e322ecf4487d24ab2484276f20f5b660e1f651df2d24623f74e216588de5fe4ee65b84cb62579e06dedb4be87bd96e0d9726ac64f6cdfd4369465b31b",
+        sHex: "01ead398135ff59757a0400074f885a7a22eb624349d3cd245309266f7229e8ca977492634b8960c2ea86215d8a9dcfb8d3e17775bcbf33cf46f1f8e8d7c3f3e4c53"
 
-    const cryptoEngine =  getCrypto(true);
+    },
+    {
+        rHex: "000000000000000000000000000000000000000000000000000000000000000000000000000000000001",
+        sHex: "000000000000000000000000000000000000000000000000000000000000000000000000000000000002"
+    },
+    {   rHex: "52e3f7b727fba9e8eddb1d083b75c1882517e6dc63ded9c0524f8f9a45dc8661",
+        sHex: "b8930438de8d33bdab12c3a2bdad979592a1fd6576d1734c3eb0af340456aef4"
+    },
+];
 
-    const caKeyPair = await cryptoEngine.subtle.generateKey( {name: "ECDSA", namedCurve: "P-521"}, true, ["sign", "verify"]);
+describe("createCMSECDSASignature - ", () => {
+    testVectors.forEach(({ rHex, sHex }, no) => {
+        it(`should encode signature vector #${no+1} correctly`, () => {
+            const signatureBuffer = Convert.FromHex(rHex + sHex);
+            const derBuffer = createCMSECDSASignature(signatureBuffer);
+            const asn1 = asn1js.fromBER(derBuffer);
 
-    const caCert = await getCACertificate(caKeyPair);
-    const certificateSigningRequest = await getCertificateSigningRequest(cryptoEngine, caKeyPair.publicKey, caKeyPair.privateKey);
+            const seq = asn1.result as asn1js.Sequence;
+            const [rInt, sInt] = seq.valueBlock.value as [asn1js.Integer, asn1js.Integer];
 
-    const certificate = new pkijs.Certificate();
-    certificate.version = 2;
-    certificate.serialNumber = new asn1js.Integer({value: 1});
+            const rValueHex = Convert.ToHex(rInt.valueBlock.valueHexView);
+            const sValueHex = Convert.ToHex(sInt.valueBlock.valueHexView);
 
-    certificate.notBefore.value = new Date();
-    certificate.notBefore.value.setDate(certificate.notBefore.value.getDate() - 1);
-
-    certificate.notAfter.value = new Date();
-    certificate.notAfter.value.setDate(certificate.notAfter.value.getDate() + 7);
-
-    certificate.subject = certificateSigningRequest.subject;
-
-    certificate.subjectPublicKeyInfo = certificateSigningRequest.subjectPublicKeyInfo;
-
-    certificate.issuer = caCert.subject;
-
-    for (let i = 1; i <= 1000; i++) {
-        console.log(`verifying certificate [${i}]`);
-        await certificate.sign(caKeyPair.privateKey, "SHA-512");
-        const verified_pkijs = await certificate.verify(caCert);
-        assert(verified_pkijs);
-
-        console.log(`verifying public key use [${i}]`);
-        const certDER = certificate.toSchema(true).toBER(false);
-        const x509Certificate = new crypto.X509Certificate(Buffer.from(certDER));
-        const spki = await cryptoEngine.subtle.exportKey("spki", caKeyPair.publicKey);
-        const nodeKey = createPublicKey({
-            key: Buffer.from(spki),
-            format: "der",
-            type: "spki",
+            assert.ok(rValueHex.endsWith(rHex.replace(/^00+/, "")), "r not encoded correctly");
+            assert.ok(sValueHex.endsWith(sHex.replace(/^00+/, "")), "s not encoded correctly");
         });
-
-        const verified_crypto = x509Certificate.verify(nodeKey);
-        assert(verified_crypto);
-    }
-});
-
-async function getCertificateSigningRequest(cryptoEngine: any, publicKey: CryptoKey, privateKey: CryptoKey) {
-    const csr = new pkijs.CertificationRequest();
-
-    csr.version = 0;
-
-    csr.subject.typesAndValues.push(new pkijs.AttributeTypeAndValue({
-        type: "2.5.4.3", // commonName
-        value: new asn1js.Utf8String({value: "Test"})
-    }));
-
-    csr.attributes = [];
-
-    await csr.subjectPublicKeyInfo.importKey(publicKey, cryptoEngine);
-
-    await csr.sign(privateKey, "SHA-512", cryptoEngine);
-
-    return csr;
-}
-
-async function  getCACertificate(caKeyPair: { publicKey: CryptoKey; privateKey: CryptoKey; }) {
-
-    const certificate = new pkijs.Certificate();
-    certificate.version = 2;
-    certificate.serialNumber = new asn1js.Integer({ value: 1 });
-    certificate.issuer.typesAndValues.push(new pkijs.AttributeTypeAndValue({
-        type: "2.5.4.3", // Common name
-        value: new asn1js.BmpString({ value: "Test" })
-    }));
-    certificate.subject.typesAndValues.push(new pkijs.AttributeTypeAndValue({
-        type: "2.5.4.3", // Common name
-        value: new asn1js.BmpString({ value: "Test" })
-    }));
-
-    certificate.notBefore.value = new Date();
-    const notAfter = new Date();
-    notAfter.setUTCFullYear(notAfter.getUTCFullYear() + 1);
-    certificate.notAfter.value = notAfter;
-
-    certificate.extensions = []; // Extensions are not a part of certificate by default, it's an optional array
-
-// "BasicConstraints" extension
-    const basicConstr = new pkijs.BasicConstraints({
-        cA: true,
-        pathLenConstraint: 3
     });
-    certificate.extensions.push(new pkijs.Extension({
-        extnID: "2.5.29.19",
-        critical: false,
-        extnValue: basicConstr.toSchema().toBER(false),
-        parsedValue: basicConstr // Parsed value for well-known extensions
-    }));
-
-// "KeyUsage" extension
-    const bitArray = new ArrayBuffer(1);
-    const bitView = new Uint8Array(bitArray);
-    bitView[0] |= 0x02; // Key usage "cRLSign" flag
-    bitView[0] |= 0x04; // Key usage "keyCertSign" flag
-    const keyUsage = new asn1js.BitString({ valueHex: bitArray });
-    certificate.extensions.push(new pkijs.Extension({
-        extnID: "2.5.29.15",
-        critical: false,
-        extnValue: keyUsage.toBER(false),
-        parsedValue: keyUsage // Parsed value for well-known extensions
-    }));
-
-// Exporting public key into "subjectPublicKeyInfo" value of certificate
-    await certificate.subjectPublicKeyInfo.importKey(caKeyPair.publicKey);
-
-// Signing final certificate
-    await certificate.sign(caKeyPair.privateKey, "SHA-512");
-
-    return certificate;
-}
+});
